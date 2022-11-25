c9c55976272f21fc888bc5aa82de6130a44e12de
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-11-24 21:09:25 -0500

    images show on home page

--- recipes/screens/account.js
+++ recipes/screens/account.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import { View, Text, Button, Alert, FlatList, Image, StyleSheet } from 'react-native';
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+import 'firebase/compat/storage'
 
 export default function AccountScreen({ navigation }) {
+  const [imagesLoaded, setImagesLoaded] = React.useState(false);
+  const [images, setImages] = React.useState(null);
+  // let images = [];
+
   let signout = () =>
   {
     firebase.auth().signOut()
@@ -11,6 +18,53 @@ export default function AccountScreen({ navigation }) {
     }).catch(error => {
       console.log('An error has occured')
     });
+  }
+
+  const getPosts = async () => {
+    let imageArr = [];
+
+    await firebase.firestore()
+    .collection("posts")
+    .doc(firebase.auth().currentUser.uid)
+    .collection("userPosts")
+    .orderBy("time", "asc")
+    .get()
+    .then(snap => {
+      snap.forEach(doc => {
+          imageArr.push(doc.data().url)
+          console.log(doc.data());
+      });
+      setImages(imageArr);
+      console.log(images);
+      setImagesLoaded(true);
+    });
+  }
+
+  let goBack = () => {
+    setImagesLoaded(false);
+  }
+
+  if (imagesLoaded !== false)
+  {
+    if(images.length === 0)
+    {
+      return (<View style={{flex:1, justifyContent:'center',alignItems:'center'}}><Text>No Posts Yet</Text><Button title="Back" onPress={goBack}/></View>)
+    }
+    return (
+      <View style={styles.container}>
+        <FlatList 
+          style={styles.list}
+          data={images}
+          renderItem={({item}) => 
+            <Image style={{height: 500, width: "100%"}} resizeMode="cover" source={{uri:item}}/>
+          }
+        />
+        <Button 
+          title="Back"
+          onPress={goBack}
+        />
+      </View>
+    );
   }
 
   return (
@@ -22,7 +76,7 @@ export default function AccountScreen({ navigation }) {
       />
       <Button 
         title="My Posts"  
-        onPress={() => Alert.alert('View posts')}
+        onPress={getPosts}
       />
       <Button 
         title="Sign out"
@@ -31,3 +85,14 @@ export default function AccountScreen({ navigation }) {
     </View>
   ); 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  },
+  list: {
+    flex:1
+  }
+})
--- recipes/screens/home.js
+++ recipes/screens/home.js
@@ -1,14 +1,71 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList, Image, StyleSheet } from 'react-native';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+import 'firebase/compat/storage'
 
 export default function HomeScreen({ navigation }) {
+
+  const [imagesLoaded, setImagesLoaded] = React.useState(false);
+  const [images, setImages] = React.useState(null);
+
+  const getPosts = async () => {
+    let imageArr = [];
+
+    await firebase.firestore()
+    .collectionGroup('userPosts')
+    .get()
+    .then(snap => {
+      snap.forEach(doc => {
+          imageArr.push(doc.data().url)
+          console.log(doc.data());
+      });
+      setImages(imageArr);
+      console.log(images);
+      setImagesLoaded(true);
+    });
+  }
+
+
+  if (imagesLoaded !== false)
+  {
     return (
-      <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View style={styles.container}>
+        <FlatList 
+          style={styles.list}
+          data={images}
+          renderItem={({item}) => 
+            <Image style={{height: 500, width: "100%"}} resizeMode="cover" source={{uri:item}}/>
+          }
+          extraData={images}
+        />
         <Button 
-          title="Account"
-          onPress={() => navigation.navigate('Account')}
+          title="Refresh"
+          onPress={getPosts}
         />
       </View>
     );
   }
+
+  return (
+    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button 
+        title="Load Posts"
+        onPress={getPosts}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  },
+  list: {
+    flex:1
+  }
+})
